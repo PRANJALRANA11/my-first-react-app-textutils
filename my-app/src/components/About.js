6140a8e2e777d54010aa5ace89554370ff5fd25c
Pranjal Rana <129268721+PRANJALRANA11@users.noreply.github.com>
2023-09-24 12:58:10 +0530

    Update About.js

--- my-app/src/components/About.js
+++ my-app/src/components/About.js
@@ -6,7 +6,7 @@ export default function About(){
   <header>
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
       <div class="container">
-        <a class="navbar-brand" href="#">Your Website</a>
+        <a class="navbar-brand" href="#">Your Websites</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
           aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
